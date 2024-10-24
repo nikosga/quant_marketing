55144839bbbce1fa07abe516c0b8c2d5cb9dce26
Nikos Gavriil <nikosga123@gmail.com>
2024-10-24 22:44:21 +0400

    lagrangian ready

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 # Objective Function
 The objective function is the maximization of profit over the set of ad channels $i \epsilon [1,...,N]$ as a function of the marketing spend $c_i$, with $q_i$ being the acquired customers through channel $i$:
 
-$$max_{c_i} \sum_i \pi_{i} (c_i) = max_{c_i} \sum_i q_i(c_i) * LTV_i - c_i$$
+$$max_{c_i} \sum_{i=1}^N \pi_{i} (c_i) = max_{c_i} \sum_{i=1}^N q_i(c_i) * LTV_i - c_i$$
+
+$$s.t. \sum_{i=1}^N c_i <= budget$$
 
 ### Acquisitions Demanded
 
@@ -12,19 +14,31 @@ We can represent diminishing returns through linear-log response curves. In that
 
 $$q_i(c_i) = b_i*log(c_i)$$
 
-$$\frac{  \partial  q_i (c_i) }{ \partial c_i } = \frac{b_i}{c_i}$$
 
-### Putting it together
-$$ max_{c_i} \sum_i b_i*log(c_i) * LTV_i - c_i $$
+### Setting up the Lagrangian Equation
+$$ L = \sum_{i=1}^N b_i*log(c_i) * LTV_i - c_i + \lambda*(budget - \sum_{i=1}^N c_i)$$
 
 # First Order Conditions
 
-$$ \partial \frac{ \sum_i b_i*log(c_i) * LTV_i - c_i}{\partial c_i} = 0 \Rightarrow$$
+For every channel the following condition applies:
 
-$$ \sum_i (\frac{LTV_i*b_i}{c_i}  - 1) = 0 \Rightarrow$$
+$$ \frac{LTV_i*b_i}{c_i}  - 1 - \lambda = 0 \, (1)$$
 
-For this to happen, all components of the sum should be zero at the same time.
+And the condition for $\lambda$
+$$ \sum_{i=1}^N c_i = budget \, (2)$$
 
-$$ \frac{LTV_i*b_i}{c_i} = 1 \Rightarrow $$
+For 2 channels:
 
-$$ c_i = LTV_i*b_i$$
+$$(1) \Rightarrow \frac{LTV_1*b_1}{c_1} = \frac{LTV_2*b_2}{c_2}\Rightarrow $$
+
+$$(2) \Rightarrow c_2 = budget - c_1$$
+
+Putting (2) into (1):
+
+$$\frac{LTV_1*b_1}{c_1} = \frac{LTV_2*b_2}{budget - c_1}\Rightarrow $$
+
+$$ \frac{budget - c_1}{c_1} =  \frac{LTV_2*b_2}{LTV_1*b_1} \Rightarrow$$
+
+$$budget = c_1 [1 + \frac{LTV_2*b_2}{LTV_1*b_1}] \Rightarrow$$
+
+$$c_1 = \frac{budget}{1 + \frac{LTV_2*b_2}{LTV_1*b_1}} , c_2 = budget - c_1$$
--- main.py
+++ main.py
@@ -42,9 +42,22 @@ def plot_profit_curve(data, x="marketing_spend", y="profit", optimal_spend=None)
                     data=data)
     if optimal_spend is not None:
         plt.axvline(optimal_spend, 0,1, color='red', linestyle='--')
-        plt.text(x=optimal_spend+0.05*(data[x].max() - data[x].min()), y=1.1*data[y].min(), s="Optimal Marketing Spend", weight="bold", color='red')
+        plt.text(x=optimal_spend+0.05*(data[x].max() - data[x].min()), y=1.1*data[y].min(), s="Optimal Spend - Simulation", weight="bold", color='red')
     plt.title('Profit vs Marketing Costs')
     plt.show()
+
+def plot_frontier(data, channel_a, channel_b, max_channel_a, max_channel_b):
+    sns.scatterplot(x=channel_a,
+                    y=channel_b,
+                    data=data,
+                    size='total profit',
+                    sizes=(10, 100))
+
+    plt.scatter(x=max_channel_a, y=max_channel_b, color='r', s=100)
+    plt.title('Frontier Plot')
+    plt.tight_layout()
+    plt.show()
+
 
 def fit_log_response_curve(data, x, y):
     model = smf.ols(formula=f'{y} ~ np.log({x}) - 1', data=data).fit()
@@ -54,7 +67,7 @@ def channel_profit(ltv, c, b, x, y):
     q = b*np.log(c)
     return q*ltv - c
 
-def grid_search_opt_profit(data, budget):
+def sim_opt_profit(data, budget, models, iterations=5000):
 
     results = {
         'iteration':[],
@@ -65,7 +78,7 @@ def grid_search_opt_profit(data, budget):
         'channel profit':[]
         }
 
-    for i in range(100):
+    for i in range(iterations):
         w = np.random.rand(4)
         total = w.sum()
         w /= total
@@ -91,9 +104,15 @@ def grid_search_opt_profit(data, budget):
 
     profit_cols = [col for col in results.columns if col.split('|')[0]=='channel profit']
     results['total profit'] = results[profit_cols]. sum(axis=1)
+
+    maxi = results['total profit'].idxmax()
+    
+    report = {}
     for channel in channels:
-        results[f'optimal spend|{channel}'] = results[f'b|{channel}']*results[f'ltv|{channel}']
-    return results
+        report[f'sim optimal spend|{channel}'] = round(results.loc[maxi, f'channel marketing spend|{channel}'],2)
+
+    report[f'sim max profit'] = round(results.loc[maxi, 'total profit'], 2)
+    return results, report
 
 if __name__=="__main__":
 
@@ -101,24 +120,40 @@ if __name__=="__main__":
     x = 'marketing_spend'
     y = 'acquisitions'
     data = gen_response_curves(x_name=x, y_name=y)
-    #plot_response_curve(data)
-
+    channels = data.channel.unique()
     # estimation
     models = {}    
-    for c in data.channel.unique():
-        #cdata = data[data.channel == c].copy()
+    for c in channels:
         model=fit_log_response_curve(data[data.channel == c], x=x, y=y)
         data.loc[data.channel == c, f'fitted_{y}'] = model.predict(data[data.channel == c])
         models[c]=model
 
     # review estimation
     plot_response_curve(data)
+    budget = 1000
+    
+    # lagrangian method
+    ltv_0 = data[data.channel == channels[0]].ltv.values[0]
+    ltv_1 = data[data.channel == channels[1]].ltv.values[0]
+    b_0 = models[channels[0]].params[f'np.log({x})']
+    b_1 = models[channels[1]].params[f'np.log({x})']
+
+    c_0 = round(budget/(1 + (ltv_1*b_1) / (ltv_0*b_0)), 2)
+    c_1 = round(budget - c_0, 2)
+    print('lagrangian optimal spend', c_0, c_1)
     
     # grid search method
-    budget = 10000
-    results = grid_search_opt_profit(data, budget)
-    print(results)
-    for channel in data.channel.unique():
-        optimal_spend = results[f'optimal spend|{channel}'].values[0]
-        print(optimal_spend)
+    #'''
+    results, report = sim_opt_profit(data, budget, models)
+    plot_frontier(results, 
+                  f"channel marketing spend|{channels[0]}", 
+                  f"channel marketing spend|{channels[1]}",
+                  report[f'sim optimal spend|{channels[0]}'],
+                  report[f'sim optimal spend|{channels[1]}']
+                  )
+    for channel in channels:
+        optimal_spend = report[f'sim optimal spend|{channel}']
         plot_profit_curve(results, x=f"channel marketing spend|{channel}", y="total profit", optimal_spend=optimal_spend)
+
+    print(report)
+    #'''
